92a54d5631f4789ded2b530900fb49f73629173f
Deepak123bharat <dsrdeepak8@gmail.com>
2019-07-19 18:10:32 +0530

    added clicking functionality for remaining shape

--- src/ClickFunctions.lua
+++ src/ClickFunctions.lua
@@ -2,6 +2,7 @@ local print = print
 local math = math
 local pairs = pairs
 local table = table
+local type = type
 local M = {}
 package.loaded[...] = M
 _ENV = M
@@ -52,6 +53,35 @@ local function RectAroundLine(x1, y1, x2, y2, x, y, h)
     end
 end
 
+local function findRemainingCoor(x1,y1,x3,y3)
+    local x2, y2, x4, y4
+    --print(x1,y1,x3,y3)
+    --print("("..type(x1)..","..type(y1)..") ("..type(x3)..","..type(y3)..")")
+
+    if x1 < x3 and y1 < y3 then
+        x2 = x1 
+        y2 = y3
+        x4 = x3
+        y4 = y1
+    elseif x1 < x3 and y1 > y3 then
+        x2 = x1
+        y2 = y3
+        x4 = x3
+        y4 = y1
+    elseif x1 > x3 and y1 < y3 then
+        x2 = x1 
+        y2 = y3
+        x4 = x3
+        y4 = y1
+    elseif x1 > x3 and y1 > y3 then
+        x2 = x1
+        y2 = y3 
+        x4 = x3
+        y4 = y1
+    end
+    return x2, y2, x4, y4
+end
+
 
 function main(cnvobj, x, y)
     local index = 0
@@ -77,47 +107,79 @@ function main(cnvobj, x, y)
                
             end
 
-            -- for rect
-            if cnvobj.drawnEle[i].shape == "RECT" then
+            -- for rect and filled rect
+            if cnvobj.drawnEle[i].shape == "RECT" or cnvobj.drawnEle[i].shape == "FILLEDRECT" then
+                
+                local x1, y1 = cnvobj.drawnEle[i].start_x, cnvobj.drawnEle[i].start_y
+                local x3, y3 = cnvobj.drawnEle[i].end_x , cnvobj.drawnEle[i].end_y
+                local x2, y2, x4, y4 = findRemainingCoor(x1, y1, x3, y3)
+
+                if cnvobj.drawnEle[i].shape == "RECT" then
+                    local i1 = RectAroundLine(x1,y1,x2,y2,x,y,cnvobj.grid_x)
+                    local i2 = RectAroundLine(x2,y2,x3,y3,x,y,cnvobj.grid_x)
+                    local i3 = RectAroundLine(x3,y3,x4,y4,x,y,cnvobj.grid_x)
+                    local i4 = RectAroundLine(x4,y4,x1,y1,x,y,cnvobj.grid_x)
+
+                    if i1==1 or i2 == 1 or i3 == 1 or i4 == 1 then
+                        cnvobj.activeEle[1] = cnvobj.drawnEle[i]
+                        table.remove(cnvobj.drawnEle, i)
+                        return i
+                    end
+                end
+
+                if cnvobj.drawnEle[i].shape == "FILLEDRECT" then
+                    local xyLiesInsideRect = check(x1, y1, x2, y2, x3, y3, x4, y4, x, y)
+                   
+                    if xyLiesInsideRect==true then
+                        cnvobj.activeEle[1] = cnvobj.drawnEle[i]
+                        table.remove(cnvobj.drawnEle, i)
+                        return i
+                    end
+                    
+                end
+
+            end
+            -- Ellipse or FILLED Ellipse
+            if cnvobj.drawnEle[i].shape == "ELLIPSE" or cnvobj.drawnEle[i].shape == "FILLEDELLIPSE" then
                 -- four coor. of rect
                 local x1, y1 = cnvobj.drawnEle[i].start_x, cnvobj.drawnEle[i].start_y
                 local x3, y3 = cnvobj.drawnEle[i].end_x , cnvobj.drawnEle[i].end_y
-                local x2, y2, x4, y4
-                --print(x1,y1,x3,y3)
-                if x1 < x3 and y1 < y3 then
-                    x2 = x1 
-                    y2 = y3
-                    x4 = x3
-                    y4 = y1
-                elseif x1 < x3 and y1 > y3 then
-                    x2 = x3
-                    y2 = y1
-                    x4 = x1
-                    y4 = y3
-                elseif x1 > x3 and y1 < y3 then
-                    x2 = x3 
-                    y2 = y1
-                    x4 = x1
-                    y4 = y3
-                elseif x1 > x3 and y1 < y3 then
-                    x2 = x3
-                    y2 = y1 
-                    x4 = x1
-                    y4 = y3
-                end
-
-                local i1 = RectAroundLine(x1,y1,x2,y2,x,y,cnvobj.grid_x)
-                local i2 = RectAroundLine(x2,y2,x3,y3,x,y,cnvobj.grid_x)
-                local i3 = RectAroundLine(x3,y3,x4,y4,x,y,cnvobj.grid_x)
-                local i4 = RectAroundLine(x4,y4,x1,y1,x,y,cnvobj.grid_x)
+                local x2, y2, x4, y4 = findRemainingCoor(x1, y1, x3, y3)
                 
-                if i1==1 or i2 == 1 or i3 == 1 or i4 == 1 then
+                midx1 = (x2 + x1)/2
+                midy1 = (y2 + y1)/2
+
+                midx2 = (x3 + x2)/2
+                midy2 = (y3 + y2)/2
+
+                midx3 = (x4 + x3)/2
+                midy3 = (y4 + y3)/2
+
+                midx4 = (x1 + x4)/2
+                midy4 = (y1 + y4)/2
+                --print("("..x1, y1..") ("..x2, y2..") ("..x3, y3..") ("..x4, y4..")")
+                --print(midx1,midy1, midx2, midy2, midx3, midy3, midx4, midy4)
+                local a = (math.abs(midx1 - midx3))/2
+                local b = (math.abs(midy2 - midy4))/2
+                
+                local cx, cy = (x1 + x3)/2 , (y1 + y3)/2
+                --print(a,b,cx,cy,x,y)
+                
+                local eq = math.pow(x-cx,2)/math.pow(a,2) + math.pow(y-cy,2)/math.pow(b,2)
+                --print(eq)
+                if cnvobj.drawnEle[i].shape == "ELLIPSE" and eq > 0.8 and eq < 1.2 then
                     cnvobj.activeEle[1] = cnvobj.drawnEle[i]
                     table.remove(cnvobj.drawnEle, i)
                     return i
                 end
 
+                if cnvobj.drawnEle[i].shape == "FILLEDELLIPSE" and eq < 1.2 then
+                    cnvobj.activeEle[1] = cnvobj.drawnEle[i]
+                    table.remove(cnvobj.drawnEle, i)
+                    return i
+                end
             end
+
         end
         return index
     end
