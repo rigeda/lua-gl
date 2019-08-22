e474583a159cdde1719a01af7b4203e266cc7c6b
Deepak123bharat <dsrdeepak8@gmail.com>
2019-08-22 16:36:53 +0530

    addport error

--- src/lua-gl.lua
+++ src/lua-gl.lua
@@ -325,8 +325,9 @@ local objFuncs = {
 			cnvobj.port[index + 1].shapeID = shapeID
 			cnvobj.port[index + 1].x = x 
 			cnvobj.port[index + 1].y = y
+			return true 
 		end
-		
+		return false
 	end, 
 
 }
